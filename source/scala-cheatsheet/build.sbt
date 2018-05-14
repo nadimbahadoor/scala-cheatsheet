012b66c34542d688ed6887be464a8e491a4f1df8
Nadim Bahadoor <nbahadoor@gmail.com>
2018-05-14 21:14:31 +0100

    Chapter 10: ScalaTest introduction, build.sbt, extending FlatSpec with Matchers, and Equality test.

--- source/scala-cheatsheet/build.sbt
+++ source/scala-cheatsheet/build.sbt
@@ -2,4 +2,6 @@ name := "scala-cheatsheet"
 
 version := "1.0"
 
-scalaVersion := "2.12.1"
+scalaVersion := "2.12.4"
+
+libraryDependencies ++= Seq("org.scalatest" %% "scalatest" % "3.0.1" % Test)
---
+++ source/scala-cheatsheet/src/test/scala/com/allaboutscala/testing/Tutorial_01_DonutTest.scala
@@ -0,0 +1,30 @@
+package com.allaboutscala.testing
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by Nadim Bahadoor on 08/04/2017.
+  *
+  *  Code snippets for ScalaTest
+  *
+  * [http://allaboutscala.com/scala-cheatsheet/#test-class-flatspec-matchers]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+class Tutorial_01_DonutTest extends FlatSpec with Matchers {
+
+  behavior of "DonutStore class"
+
+}
