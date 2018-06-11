0230dbbaea1460cc91210607d88f77bde5829b7e
Nadim Bahadoor <nbahadoor@gmail.com>
2018-06-11 21:21:03 +0100

    Chapter 10: ScalaTest Private Method Test.

--- source/scala-cheatsheet/src/test/scala/com/allaboutscala/testing/Tutorial_08_Private_Method_Test.scala
+++ source/scala-cheatsheet/src/test/scala/com/allaboutscala/testing/Tutorial_08_Private_Method_Test.scala
@@ -1,5 +1,7 @@
 package com.allaboutscala.testing
 
+import org.scalatest.{Matchers, FlatSpec}
+
 /**
   * Created by Nadim Bahadoor on 08/04/2017.
   *
@@ -21,32 +23,18 @@ package com.allaboutscala.testing
   * License for the specific language governing permissions and limitations under
   * the License.
   */
-class DonutStore {
+class Tutorial_08_Private_Method_Test extends FlatSpec with Matchers {
 
-  def favouriteDonut(): String = "vanilla donut"
+  "Example of testing private method" should "be valid" in {
+    val donutStore = new DonutStore()
+    val priceWithDiscount = donutStore.donutPrice("vanilla donut")
+    priceWithDiscount shouldEqual Some(1.6)
 
-  def donuts(): Seq[String] = Seq("vanilla donut", "plain donut", "glazed donut")
-
-  def printName(): Unit = {
-    throw new IllegalStateException("Some Error")
+    // test the private method discountByDonut()
+    import org.scalatest.PrivateMethodTester._
+    val discountByDonutMethod = PrivateMethod[Double]('discountByDonut)
+    val vanillaDonutDiscount = donutStore invokePrivate discountByDonutMethod("vanilla donut")
+    vanillaDonutDiscount shouldEqual 0.2
   }
 
-  def donutPrice(donut: String): Option[Double] = {
-    val prices = Map(
-      "vanilla donut" -> 2.0,
-      "plain donut"   -> 1.0,
-      "glazed donut"  -> 3.0
-    )
-    val priceOfDonut = prices.get(donut)
-    priceOfDonut.map{ price => price * (1 - discountByDonut(donut)) }
-  }
-
-  private def discountByDonut(donut: String): Double = {
-    val discounts = Map(
-      "vanilla donut" -> 0.2,
-      "plain donut"   -> 0.1,
-      "glazed donut"  -> 0.3
-    )
-    discounts.getOrElse(donut, 0)
-  }
 }
